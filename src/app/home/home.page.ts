8a8a0af26ef6fed188e794a9d98780e568a423fb
Pamela <pa.romerov@duocuc.cl>
2024-11-09 10:59:43 -0300

    Primer commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { logoIonic } from 'ionicons/icons';
+import { ActivatedRoute } from '@angular/router'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  receivedUsername!: string;
+
+  constructor(private route: ActivatedRoute) {
+    addIcons({ logoIonic });
+  }
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.receivedUsername = params['user']; 
+    });
+  }
+  }
+
+
