e71ab50c15cc38e75f86f0cb228407cbff8136c0
Karina Tobar <kar.tobar@duocuc.cl>
2024-11-23 13:39:38 -0300

    Mensaje de actualización

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,19 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from '../environments/environment';
+
+import { CrudfirebaseService } from './servicios/crudfirebase.service';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth'; 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
+  declarations: [AppComponent ],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule, HttpClientModule,FormsModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync(), CrudfirebaseService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
